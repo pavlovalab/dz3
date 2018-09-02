6508d1b459c62c78e3d301c995bc95184a1e7b45
Александра <pavlovalab@gmail.com>
2018-09-02 19:22:28 +0300

    ura

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -12,48 +13,78 @@ public class Test {
         WebDriver driver = getInitFirefoxDriver();
         driver.manage().window().maximize();
 
+        //      EventFiringWebDriver edriver = new EventFiringWebDriver(driver);
+        //      MyLogger eventListener = new MyLogger(edriver);
+        //      edriver.register(eventListener);
+
         driver.get("http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0/");
 
-        WebElement adressEmail = driver.findElement(By.id("email"));
-        adressEmail.sendKeys("webinar.test@gmail.com");
+        driver.findElement(By.id("email")).sendKeys("webinar.test@gmail.com");
 
         Thread.sleep(1000);
 
-        WebElement password = driver.findElement(By.id("passwd"));
-        password.sendKeys("Xcg7299bnSmMuRLp9ITw");
+        driver.findElement(By.id("passwd")).sendKeys("Xcg7299bnSmMuRLp9ITw");
 
         Thread.sleep(1000);
 
-        WebElement button = driver.findElement(By.name("submitLogin"));
-        button.click();
+        driver.findElement(By.name("submitLogin")).click();
 
-        new WebDriverWait(driver,10).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#subtab-AdminCategories[data-submenu='11']")));
+        //ОЖИДАНИЕ ПОЯВЛЕНИЯ МЕНЮ
+        new WebDriverWait(driver,10).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#subtab-AdminCatalog[data-submenu='9']")));
+
         WebElement catalog = driver.findElement(By.cssSelector("#subtab-AdminCatalog[data-submenu='9']"));
-        Actions builder = new Actions(driver);
-        builder.moveToElement(catalog).perform();
+        WebElement orders = driver.findElement(By.cssSelector("#subtab-AdminParentOrders[data-submenu='3']"));
 
-        new WebDriverWait(driver,10).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#subtab-AdminCategories[data-submenu='11']")));
+        Actions builder = new Actions(driver);
+        do {
+            builder.moveToElement(catalog).perform();
+            try {
+                // Ожидание появление подменю
+                new WebDriverWait(driver,1).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#subtab-AdminCategories[data-submenu='11']")));
+                break;
+            }catch (Exception e){
+                System.out.println("Не готово");
+            }
+            builder.moveToElement(orders).perform();
+        }while (1==1);
+
         WebElement category = driver.findElement(By.cssSelector("#subtab-AdminCategories[data-submenu='11']"));
         builder.moveToElement(category).click().perform();
 
+        //Ожидание страницы управления категориями
         new WebDriverWait(driver,10).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#page-header-desc-category-new_category[title='Добавить категорию']")));
-        WebElement add_category = driver.findElement(By.cssSelector("#page-header-desc-category-new_category[title='Добавить категорию']"));
-        add_category.click();
+        driver.findElement(By.cssSelector("#page-header-desc-category-new_category[title='Добавить категорию']")).click();
 
-        new WebDriverWait(driver,10).until(ExpectedConditions.presenceOfElementLocated(By.id("name_1")));
-        WebElement name_category = driver.findElement(By.id("name_1"));
-        name_category.sendKeys("Fruits");
+        //заполнение имя новой категории
+        new WebDriverWait(driver,10).until(ExpectedConditions.presenceOfElementLocated(By.id("link_rewrite_1")));
+        driver.findElement(By.id("name_1")).sendKeys("Fruits");
 
         Thread.sleep(1000);
 
-        WebElement button_save = driver.findElement(By.id("category_form_submit_btn"));
-        button_save.click();
-        new WebDriverWait(driver,10).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#content>.bootstrap>.alert")));
+        //нажимаем на кнопку сохранить
+        driver.findElement(By.id("category_form_submit_btn")).click();
 
-        WebElement success = driver.findElement(By.cssSelector("#content>.bootstrap>.alert"));
-        String alert = success.getAttribute("class");
+        //ожидание появления сообщения
+        new WebDriverWait(driver,10).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#content>.bootstrap>.alert")));
+        String alert = driver.findElement(By.cssSelector("#content>.bootstrap>.alert")).getAttribute("class");
         if(alert.equals("alert alert-success")){
             System.out.println("Категория успешно создана");
+
+            //поиск по имени в фильтре
+            driver.findElement(By.name("categoryFilter_name")).sendKeys("Fruits");
+
+            //поиск и нажатие на кнопку "Поиск"
+            driver.findElement(By.id("submitFilterButtoncategory")).click();
+
+            //ожидание появления таблицы категорий
+            new WebDriverWait(driver,10).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#table-category")));
+
+            //поиск созданной категории в таблице
+            if(!driver.findElements(By.xpath("//td[preceding-sibling::td[contains(.,'Fruits')]]")).isEmpty()){
+                System.out.println("Запись созданной категории присутствует");
+            }else{
+                System.out.println("Запись созданной категории отсутствует");
+            }
         }else{
             System.out.println("Ошибка создания категории");
         }
@@ -61,213 +92,6 @@ public class Test {
         Thread.sleep(5000);
         driver.quit();
 
-     /*   catalog.click();
-        new WebDriverWait(driver,10).until(ExpectedConditions.presenceOfElementLocated(By.cssSel
-
-                Thread.sleep(3000);
-        razdel = driver.findElement(By.tagName("h2")).getText();
-        System.out.println(razdel);
-        driver.navigate().refresh();
-
-        if (driver.findElement(By.tagName("h2")).getText().equals(razdel)==false){
-            System.out.println("Ошибка.Произошел переход в другой раздел!");
-        }else {
-            System.out.println("Пользователь остается в том же разделе!");
-        }
-
-        /*WebElement pictureUser = driver.findElement(By.cssSelector("#header_employee_box span"));
-        pictureUser.click();
-
-        Thread.sleep(1000);
-
-        WebElement logout = driver.findElement(By.id("header_logout"));
-        logout.click();
-
-        Thread.sleep(3000);
-
-        WebElement adressEmail1 = driver.findElement(By.id("email"));
-        adressEmail1.sendKeys("webinar.test@gmail.com");
-
-        Thread.sleep(1000);
-
-        WebElement password1 = driver.findElement(By.id("passwd"));
-        password1.sendKeys("Xcg7299bnSmMuRLp9ITw");
-
-        Thread.sleep(1000);
-
-        WebElement button1 = driver.findElement(By.name("submitLogin"));
-        button1.click();
-
-        Thread.sleep(4000);
-
-        WebElement dashboard = driver.findElement(By.cssSelector("#tab-AdminDashboard span"));
-        dashboard.click();
-        Thread.sleep(2000);
-        String razdel = driver.findElement(By.className("page-title")).getText();
-        System.out.println(razdel);
-        driver.navigate().refresh();
-
-        if (driver.findElement(By.className("page-title")).getText().equals(razdel)==false){
-            System.out.println("Ошибка.Произошел переход в другой раздел!");
-        }else {
-            System.out.println("Пользователь остается в том же разделе!");
-        }
-
-        WebElement zakazy = driver.findElement(By.cssSelector("#subtab-AdminParentOrders span"));
-        zakazy.click();
-        Thread.sleep(2000);
-        razdel = driver.findElement(By.className("page-title")).getText();
-        System.out.println(razdel);
-        driver.navigate().refresh();
-
-        if (driver.findElement(By.className("page-title")).getText().equals(razdel)==false){
-            System.out.println("Ошибка.Произошел переход в другой раздел!");
-        }else {
-            System.out.println("Пользователь остается в том же разделе!");
-        }
-
-        WebElement catalog = driver.findElement(By.cssSelector("#subtab-AdminCatalog span"));
-        catalog.click();
-        Thread.sleep(3000);
-        razdel = driver.findElement(By.tagName("h2")).getText();
-        System.out.println(razdel);
-        driver.navigate().refresh();
-
-        if (driver.findElement(By.tagName("h2")).getText().equals(razdel)==false){
-            System.out.println("Ошибка.Произошел переход в другой раздел!");
-        }else {
-            System.out.println("Пользователь остается в том же разделе!");
-        }
-
-        WebElement customers = driver.findElement(By.cssSelector(".link-levelone[data-submenu='23']"));
-        customers.click();
-        Thread.sleep(2000);
-        razdel = driver.findElement(By.className("page-title")).getText();
-        System.out.println(razdel);
-        driver.navigate().refresh();
-
-        if (driver.findElement(By.className("page-title")).getText().equals(razdel)==false){
-            System.out.println("Ошибка.Произошел переход в другой раздел!");
-        }else {
-            System.out.println("Пользователь остается в том же разделе!");
-        }
-
-        WebElement customerThreads = driver.findElement(By.cssSelector("#subtab-AdminParentCustomerThreads span"));
-        customerThreads.click();
-        Thread.sleep(2000);
-        razdel = driver.findElement(By.className("page-title")).getText();
-        System.out.println(razdel);
-        driver.navigate().refresh();
-
-        if (driver.findElement(By.className("page-title")).getText().equals(razdel)==false){
-            System.out.println("Ошибка.Произошел переход в другой раздел!");
-        }else {
-            System.out.println("Пользователь остается в том же разделе!");
-        }
-
-        WebElement stats = driver.findElement(By.cssSelector("#subtab-AdminStats span"));
-        stats.click();
-        Thread.sleep(2000);
-        razdel = driver.findElement(By.className("page-title")).getText();
-        System.out.println(razdel);
-        driver.navigate().refresh();
-
-        if (driver.findElement(By.className("page-title")).getText().equals(razdel)==false){
-            System.out.println("Ошибка.Произошел переход в другой раздел!");
-        }else {
-            System.out.println("Пользователь остается в том же разделе!");
-        }
-
-        WebElement modules = driver.findElement(By.cssSelector("#subtab-AdminParentModulesSf span"));
-        modules.click();
-        Thread.sleep(20000);
-        razdel = driver.findElement(By.tagName("h2")).getText();
-        System.out.println(razdel);
-        driver.navigate().refresh();
-
-        if (driver.findElement(By.tagName("h2")).getText().equals(razdel)==false){
-            System.out.println("Ошибка.Произошел переход в другой раздел!");
-        }else {
-            System.out.println("Пользователь остается в том же разделе!");
-        }
-
-        WebElement design = driver.findElement(By.cssSelector(".link-levelone[data-submenu='46']"));
-        design.click();
-        Thread.sleep(1000);
-        razdel = driver.findElement(By.className("page-title")).getText();
-        System.out.println(razdel);
-        driver.navigate().refresh();
-
-        if (driver.findElement(By.className("page-title")).getText().equals(razdel)==false){
-            System.out.println("Ошибка.Произошел переход в другой раздел!");
-        }else {
-            System.out.println("Пользователь остается в том же разделе!");
-        }
-
-        WebElement dostavka = driver.findElement(By.cssSelector("#subtab-AdminParentShipping span"));
-        dostavka.click();
-        Thread.sleep(2000);
-        razdel = driver.findElement(By.className("page-title")).getText();
-        System.out.println(razdel);
-        driver.navigate().refresh();
-
-        if (driver.findElement(By.className("page-title")).getText().equals(razdel)==false){
-            System.out.println("Ошибка.Произошел переход в другой раздел!");
-        }else {
-            System.out.println("Пользователь остается в том же разделе!");
-        }
-
-        WebElement payment = driver.findElement(By.cssSelector("#subtab-AdminParentPayment span"));
-        payment.click();
-        Thread.sleep(2000);
-        razdel = driver.findElement(By.className("page-title")).getText();
-        System.out.println(razdel);
-        driver.navigate().refresh();
-
-        if (driver.findElement(By.className("page-title")).getText().equals(razdel)==false){
-            System.out.println("Ошибка.Произошел переход в другой раздел!");
-        }else {
-            System.out.println("Пользователь остается в том же разделе!");
-        }
-
-        WebElement international = driver.findElement(By.cssSelector("#subtab-AdminInternational span"));
-        international.click();
-        Thread.sleep(2000);
-        razdel = driver.findElement(By.className("page-title")).getText();
-        System.out.println(razdel);
-        driver.navigate().refresh();
-
-        if (driver.findElement(By.className("page-title")).getText().equals(razdel)==false){
-            System.out.println("Ошибка.Произошел переход в другой раздел!");
-        }else {
-            System.out.println("Пользователь остается в том же разделе!");
-        }
-
-        WebElement shopParameters = driver.findElement(By.cssSelector("#subtab-ShopParameters span"));
-        shopParameters.click();
-        Thread.sleep(2000);
-        razdel = driver.findElement(By.className("page-title")).getText();
-        System.out.println(razdel);
-        driver.navigate().refresh();
-
-        if (driver.findElement(By.className("page-title")).getText().equals(razdel)==false){
-            System.out.println("Ошибка.Произошел переход в другой раздел!");
-        }else {
-            System.out.println("Пользователь остается в том же разделе!");
-        }
-
-        WebElement advancedParameters = driver.findElement(By.cssSelector("#subtab-AdminAdvancedParameters span"));
-        advancedParameters.click();
-        Thread.sleep(2000);
-        razdel = driver.findElement(By.className("page-title")).getText();
-        System.out.println(razdel);
-        driver.navigate().refresh();
-
-        if (driver.findElement(By.className("page-title")).getText().equals(razdel)==false){
-            System.out.println("Ошибка.Произошел переход в другой раздел!");
-        }else {
-            System.out.println("Пользователь остается в том же разделе!");
-        }*/
     }
     public static WebDriver getInitFirefoxDriver(){
         System.setProperty("webdriver.gecko.driver",Test.class.getResource ("geckodriver.exe").getPath());
