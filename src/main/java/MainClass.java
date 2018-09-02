407b5cbbd37a58f044d69ffccbb6a44aceb06bd2
Александра <pavlovalab@gmail.com>
2018-09-02 20:30:09 +0300

    ura

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -8,17 +8,18 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class Test {
+public class MainClass {
     public static void main(String[] args) throws InterruptedException {
-        WebDriver driver = getInitFirefoxDriver();
-        driver.manage().window().maximize();
+        WebDriver fdriver = getInitFirefoxDriver();
+        fdriver.manage().window().maximize();
 
-        //      EventFiringWebDriver edriver = new EventFiringWebDriver(driver);
-        //      MyLogger eventListener = new MyLogger(edriver);
-        //      edriver.register(eventListener);
+        EventFiringWebDriver driver = new EventFiringWebDriver(fdriver);
+        MyLogger eventListener = new MyLogger();
+        driver.register(eventListener);
 
         driver.get("http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0/");
 
+        eventListener.debug("Войти в Админ Панель:");
         driver.findElement(By.id("email")).sendKeys("webinar.test@gmail.com");
 
         Thread.sleep(1000);
@@ -29,8 +30,9 @@ public class Test {
 
         driver.findElement(By.name("submitLogin")).click();
 
-        //ОЖИДАНИЕ ПОЯВЛЕНИЯ МЕНЮ
-        new WebDriverWait(driver,10).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#subtab-AdminCatalog[data-submenu='9']")));
+        eventListener.debug("Выбрать пункт меню Каталог -> категории:");
+        //Ожидание появления меню
+        new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#subtab-AdminCatalog[data-submenu='9']")));
 
         WebElement catalog = driver.findElement(By.cssSelector("#subtab-AdminCatalog[data-submenu='9']"));
         WebElement orders = driver.findElement(By.cssSelector("#subtab-AdminParentOrders[data-submenu='3']"));
@@ -40,23 +42,25 @@ public class Test {
             builder.moveToElement(catalog).perform();
             try {
                 // Ожидание появление подменю
-                new WebDriverWait(driver,1).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#subtab-AdminCategories[data-submenu='11']")));
+                new WebDriverWait(driver, 1).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#subtab-AdminCategories[data-submenu='11']")));
                 break;
-            }catch (Exception e){
-                System.out.println("Не готово");
+            } catch (Exception e) {
+                eventListener.debug("Ожидание появление подменю!");
             }
             builder.moveToElement(orders).perform();
-        }while (1==1);
+        } while (1 == 1);
 
         WebElement category = driver.findElement(By.cssSelector("#subtab-AdminCategories[data-submenu='11']"));
         builder.moveToElement(category).click().perform();
 
+        eventListener.debug("Нажать «Добавить категорию»:");
         //Ожидание страницы управления категориями
-        new WebDriverWait(driver,10).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#page-header-desc-category-new_category[title='Добавить категорию']")));
+        new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#page-header-desc-category-new_category[title='Добавить категорию']")));
         driver.findElement(By.cssSelector("#page-header-desc-category-new_category[title='Добавить категорию']")).click();
 
+        eventListener.debug("Ввести название новой категории и сохранить изменения:");
         //заполнение имя новой категории
-        new WebDriverWait(driver,10).until(ExpectedConditions.presenceOfElementLocated(By.id("link_rewrite_1")));
+        new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.id("link_rewrite_1")));
         driver.findElement(By.id("name_1")).sendKeys("Fruits");
 
         Thread.sleep(1000);
@@ -65,11 +69,12 @@ public class Test {
         driver.findElement(By.id("category_form_submit_btn")).click();
 
         //ожидание появления сообщения
-        new WebDriverWait(driver,10).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#content>.bootstrap>.alert")));
+        new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#content>.bootstrap>.alert")));
         String alert = driver.findElement(By.cssSelector("#content>.bootstrap>.alert")).getAttribute("class");
-        if(alert.equals("alert alert-success")){
-            System.out.println("Категория успешно создана");
+        if (alert.equals("alert alert-success")) {
+            eventListener.debug("Категория успешно создана!");
 
+            eventListener.debug("Отфильтровать таблицу категорий по имени:");
             //поиск по имени в фильтре
             driver.findElement(By.name("categoryFilter_name")).sendKeys("Fruits");
 
@@ -77,24 +82,25 @@ public class Test {
             driver.findElement(By.id("submitFilterButtoncategory")).click();
 
             //ожидание появления таблицы категорий
-            new WebDriverWait(driver,10).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#table-category")));
+            new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#table-category")));
 
             //поиск созданной категории в таблице
-            if(!driver.findElements(By.xpath("//td[preceding-sibling::td[contains(.,'Fruits')]]")).isEmpty()){
-                System.out.println("Запись созданной категории присутствует");
-            }else{
-                System.out.println("Запись созданной категории отсутствует");
+            if (!driver.findElements(By.xpath("//td[preceding-sibling::td[contains(.,'Fruits')]]")).isEmpty()) {
+                eventListener.debug("Запись созданной категории присутствует!");
+            } else {
+                eventListener.debug("Запись созданной категории отсутствует!");
             }
-        }else{
-            System.out.println("Ошибка создания категории");
+        } else {
+            eventListener.debug("Ошибка создания категории!");
         }
 
         Thread.sleep(5000);
         driver.quit();
-
+        driver.unregister(eventListener);
     }
-    public static WebDriver getInitFirefoxDriver(){
-        System.setProperty("webdriver.gecko.driver",Test.class.getResource ("geckodriver.exe").getPath());
+
+    public static WebDriver getInitFirefoxDriver() {
+        System.setProperty("webdriver.gecko.driver", MainClass.class.getResource("geckodriver.exe").getPath());
         return new FirefoxDriver();
     }
 }
---
+++ src/main/java/MyLogger.java
@@ -0,0 +1,96 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.events.WebDriverEventListener;
+
+
+public class MyLogger implements WebDriverEventListener {
+
+    public void debug(String str) {
+        System.out.println(str);
+    }
+
+    public void afterAlertAccept(WebDriver var1) {
+
+    }
+
+    public void beforeAlertAccept(WebDriver var1) {
+
+    }
+
+    public void afterAlertDismiss(WebDriver var1) {
+
+    }
+
+    public void beforeAlertDismiss(WebDriver var1) {
+
+    }
+
+    public void afterNavigateRefresh(WebDriver var1) {
+
+    }
+
+    public void beforeNavigateTo(String url, WebDriver driver) {
+        System.out.print("Переход на страницу " + url);
+    }
+
+    public void afterNavigateTo(String url, WebDriver driver) {
+        System.out.println(" - выполнен");
+    }
+
+    public void beforeNavigateBack(WebDriver driver) {
+
+    }
+
+    public void afterNavigateBack(WebDriver driver) {
+
+    }
+
+    public void beforeNavigateForward(WebDriver driver) {
+
+    }
+
+    public void afterNavigateForward(WebDriver driver) {
+
+    }
+
+    public void beforeNavigateRefresh(WebDriver var1) {
+
+    }
+
+    public void beforeFindBy(By by, WebElement element, WebDriver driver) {
+        System.out.println("Поиск элемента по " + by + "");
+    }
+
+    public void afterFindBy(By by, WebElement element, WebDriver driver) {
+        
+    }
+
+    public void beforeClickOn(WebElement element, WebDriver driver) {
+        System.out.print("Click по " + element.getTagName());
+    }
+
+    public void afterClickOn(WebElement element, WebDriver driver) {
+        System.out.println(" - Click выполнен");
+    }
+
+    public void afterChangeValueOf(WebElement var1, WebDriver var2, CharSequence[] var3) {
+
+    }
+
+    public void beforeChangeValueOf(WebElement var1, WebDriver var2, CharSequence[] var3) {
+
+    }
+
+    public void beforeScript(String script, WebDriver driver) {
+
+    }
+
+    public void afterScript(String script, WebDriver driver) {
+
+    }
+
+    public void onException(Throwable throwable, WebDriver driver) {
+
+    }
+}
